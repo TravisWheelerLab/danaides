6725fb0278e2663700dd4a4f7465d68a53d33cc9
Max Copeland <maxcopeland88@gmail.com>
2024-06-05 10:44:37 -0700

    Restructure policies, add ddb, s3, cloudwatch, sqs policies

--- modules/app/iam.tf
+++ modules/app/iam.tf
@@ -1,14 +1,5 @@
-data "aws_iam_policy" "lambda_execute_policy" {
-  arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
-}
 
-data "aws_iam_policy" "lambda_vpc_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
 
-data "aws_iam_policy" "efs_full_access_policy" {
-  arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
-}
 
 resource "aws_iam_role" "lambda_efs_role" {
   name = "lambda_efs_role"
@@ -26,9 +17,17 @@ resource "aws_iam_role" "lambda_efs_role" {
   })
 }
 
+data "aws_iam_policy" "lambda_execute_policy" {
+  arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_execute_policy_attachment" {
   role       = aws_iam_role.lambda_efs_role.name
   policy_arn = data.aws_iam_policy.lambda_execute_policy.arn
+}
+
+data "aws_iam_policy" "lambda_vpc_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attachment" {
@@ -36,8 +35,47 @@ resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attachment" {
   policy_arn = data.aws_iam_policy.lambda_vpc_policy.arn
 }
 
+data "aws_iam_policy" "efs_full_access_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "efs_full_access_policy_attachment" {
   role       = aws_iam_role.lambda_efs_role.name
   policy_arn = data.aws_iam_policy.efs_full_access_policy.arn
 }
 
+data "aws_iam_policy" "cloudwatch_logs_policy" {
+  arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy_attachment" {
+  role       = aws_iam_role.lambda_efs_role.name
+  policy_arn = data.aws_iam_policy.cloudwatch_logs_policy.arn
+}
+
+data "aws_iam_policy" "s3_full_access_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_policy_attachment" {
+  role       = aws_iam_role.lambda_efs_role.name
+  policy_arn = data.aws_iam_policy.s3_full_access_policy.arn
+}
+
+data "aws_iam_policy" "ddb_full_access_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ddb_full_access_policy_attachment" {
+  role       = aws_iam_role.lambda_efs_role.name
+  policy_arn = data.aws_iam_policy.ddb_full_access_policy.arn
+}
+
+data "aws_iam_policy" "lambda_sqs_execution_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sqs_execution_policy_attachment" {
+  role       = aws_iam_role.lambda_efs_role.name
+  policy_arn = data.aws_iam_policy.lambda_sqs_execution_policy.arn
+}
